9bc73ef288ed959bbdbb94be7da48ec85c8f2b6b
Tom Brewe <tom@nylkiway.net>
2024-05-31 15:22:04 +0200

    chore: update build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 mkdir -p build
-nim c --out:build/pinholeCalculator -d:release -d:adwmajor=1 -d:adwminor=4 src/main.nim
+nim c --out:build/pinholeExposureCalculator -d:release -d:adwmajor=1 -d:adwminor=4 src/main.nim
--- startDev.sh
+++ startDev.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 mkdir -p build
-nim c  --out:build/pinholeCalculatorDev -r -d:adwmajor=1 -d:adwminor=4 src/main.nim
+nim c  --out:build/pinholeExposureCalculatorDev -r -d:adwmajor=1 -d:adwminor=4 src/main.nim
